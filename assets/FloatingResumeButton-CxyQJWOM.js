c6921e129b2546117a589e992a74246cbe2f9653
kkamal11 <kishorkamal7091@gmail.com>
2026-02-25 06:36:45 +0530

    Updates

--- assets/FloatingResumeButton-CxyQJWOM.js
+++
@@ -1,4 +0,0 @@
-import{r as t,j as r,L as o}from"./index-DlfX1smr.js";function i(){const[e,a]=t.useState(!1);return t.useEffect(()=>{const s=setTimeout(()=>{a(!0)},1500);return()=>clearTimeout(s)},[]),r.jsx(o,{to:"/resume",className:`flex fixed right-5 bottom-1 -translate-y-1/2 z-50
-      px-3 py-2 bg-black text-white rounded shadow-lg
-      hover:bg-gray-900 transition-all duration-500
-      ${e?"opacity-100 translate-x-0":"opacity-0 translate-x-10"}`,children:"📄 Resume"})}export{i as default};
