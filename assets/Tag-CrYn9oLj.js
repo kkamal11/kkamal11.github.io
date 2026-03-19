9b6ad7f35f12ddc0e08fd55ebf4fac2161b437fe
kkamal11 <kishorkamal7091@gmail.com>
2026-03-19 18:00:20 +0530

    Updates

---
+++ assets/Tag-CrYn9oLj.js
@@ -0,0 +1,7 @@
+import{j as t}from"./index-DQQiCVBM.js";const i=({slotValue:e,doTransition:r=!1})=>t.jsx("span",{className:`
+        inline-flex items-center flex-shrink-0
+        px-2 py-1 rounded-[2px]
+        text-[8px] tracking-widest uppercase
+        bg-white border border-[#e8e6df] text-gray-500
+        ${r?"cursor-pointer transition-all duration-150 hover:bg-[#1a1916] hover:border-[#1a1916] hover:text-white":""}
+      `,children:e});export{i as Tag};
